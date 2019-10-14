8ec57e176f1cb13cf5f22038be1b8d7c999b6820
Filipe Varjão <varjaofilipe@gmail.com>
2019-10-14 10:51:22 -0300

    first

---
+++ lib/ex_hater.ex
@@ -0,0 +1,18 @@
+defmodule ExHater do
+  @moduledoc """
+  Documentation for ExHater.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExHater.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_hater_test.exs
@@ -0,0 +1,8 @@
+defmodule ExHaterTest do
+  use ExUnit.Case
+  doctest ExHater
+
+  test "greets the world" do
+    assert ExHater.hello() == :world
+  end
+end
