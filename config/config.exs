46d452297fa7f1310b51f47d2351027e51220548
Filipe Varjão <varjaofilipe@gmail.com>
2019-11-26 16:31:26 -0300

    [WIP] add test for server

--- config/config.exs
+++ config/config.exs
@@ -6,3 +6,11 @@ config :extwitter, :oauth,
   access_token: "",
   access_token_secret: "",
   screen_name: ""
+
+config :ex_watson_tone, url: "https://gateway.watsonplatform.net/tone-analyzer/api"
+config :ex_watson_tone, version: System.get_env("VERSION") || "2017-09-21"
+
+config :ex_watson_tone,
+  api_key: System.get_env("APIKEY")
+
+config :mnesia, dir: 'mnesia/#{Mix.env()}/#{node()}'
